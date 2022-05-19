e273ae59bb347e793bf400c58139f173b3d85425
shdhrai <shdhrai@gmail.com>
2022-05-19 21:19:12 +0530

    Second Commit

--- src/Component/LeaderBoard.js
+++ src/Component/LeaderBoard.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { response } from '../response';
 
 const index = 0;
 
 function LeaderBoard(props) {
-	const { rank, name, points, age} = response.list[index];
+	//const { rank, name, points, age} = response.list[index];
+	const[data,setdata]= useState(response.list)
+	const sorting=(col)=>{
+		if(col==="rank"){
+			const sorted = [...data].sort((a, b) => a.rank - b.rank);
+			
+			setdata(sorted);
+		}
+		if(col==="name"){
+			const sorted = [...data].sort((a, b) => {
+				return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
+			});
+			setdata(sorted);
+		}
+		if(col==="points"){
+			const sorted = [...data].sort((a, b) => a.points - b.points);
+			setdata(sorted);
+		}
+		if(col==="age"){
+			const sorted = [...data].sort((a, b) => a.age - b.age);
+			setdata(sorted);
+		}
+	}
 	return (
 		<div className="text-center mt-50">
 			<div>
 				<div>
-					<button data-testid="route-rank" className='outlined' type="button">Rank</button>
-					<button data-testid="route-name" className='outlined' type="button">Name</button>
-					<button data-testid="route-points" className='outlined' type="button">Points</button>
-					<button data-testid="route-age" className='outlined' type="button">Age</button>
+					<button data-testid="route-rank" className='outlined' type="button" onClick={()=>sorting("rank")}>Rank</button>
+					<button data-testid="route-name" className='outlined' type="button" onClick={()=>sorting("name")}>Name</button>
+					<button data-testid="route-points" className='outlined' type="button" onClick={()=>sorting("points")}>Points</button>
+					<button data-testid="route-age" className='outlined' type="button" onClick={()=>sorting("age")}>Age</button>
 				</div>
 			</div>
 			<div className="card mx-auto pb-20 mb-30" style={{ width: '50%' }}>
@@ -25,13 +47,16 @@ function LeaderBoard(props) {
 							<th className="numeric">Age</th>
 						</tr>
 					</thead>
+				
 					<tbody data-testid="app-tbody">
-						<tr key={rank}>
-							<td data-testid={`rank-${index}`}>{rank}</td>
-							<td data-testid={`name-${index}`}>{name}</td>
-							<td data-testid={`points-${index}`} className="numeric">{points}</td>
-							<td data-testid={`age-${index}`} className="numeric">{age}</td>
+					{data.map(elem=>(
+						<tr key={elem.id}>
+							<td data-testid={`rank-${index}`}>{elem.rank}</td>
+							<td data-testid={`name-${index}`}>{elem.name}</td>
+							<td data-testid={`points-${index}`} className="numeric">{elem.points}</td>
+							<td data-testid={`age-${index}`} className="numeric">{elem.age}</td>
 						</tr>
+						))}	
 					</tbody>
 				</table>
 			</div>
